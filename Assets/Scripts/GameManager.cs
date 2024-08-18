09c8e146d61dc55c00fd230662e4d651ebfe4198
hiroglyph22 <hirotrepreneur@gmail.com>
2024-08-17 23:23:34 -0700

    Added start button that starts point system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
+    public Button startButton;
     static public bool gameRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameRunning = true;
+        startButton.onClick.AddListener(TaskOnStart);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TaskOnStart()
+    {
+        gameRunning = true;
     }
 }
