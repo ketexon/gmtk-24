7eb13378a62758da71830c2889e666e6b311e5c1
ketexon <zane.a.s.clark@gmail.com>
2024-08-17 23:28:21 -0700

    fixup! Added time-based point system

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track : MonoBehaviour
+{
+    [SerializeField] TrackSegment idleSegments;
+    [SerializeField] TrackSegment obstacleSegments;
+
+    List<TrackSegment> spawnedSegments = new();
+
+    void Start()
+    {
+        idleSegments.
+    }
+}
