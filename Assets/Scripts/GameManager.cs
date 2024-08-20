62dbdd2bb818c29750499466993c4ba4c3511b41
hiroglyph22 <hirotrepreneur@gmail.com>
2024-08-20 01:45:46 -0700

    Added start functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
 
     static public bool gameRunning = false;
+    public Button startButton;
+    public CinemachineVirtualCamera startingVCam;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameRunning = true;
+        startButton.onClick.AddListener(StartPlay);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartPlay()
+    {
+        gameRunning = true;
+        startingVCam.enabled = false;
     }
 }
