25e1e0a7020fa21ae80d2bfcdbc4213effe2293a
Aubrey Clark <zane.a.s.clark@gmail.com>
2024-08-20 01:17:19 -0700

    fixed up obstacles

--- Assets/Scripts/StaticBalloonResizable.cs
+++ Assets/Scripts/StaticBalloonResizable.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(Balloon))]
 public class StaticBalloonResizable : BalloonResizable
 {
+    Rigidbody rb;
+
+    override protected void Awake(){
+        base.Awake();
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    public override void Enlarge()
+    {
+        base.Enlarge();
+        rb.isKinematic = false;
+    }
+
+    public override void Shrink()
+    {
+        base.Shrink();
+        rb.isKinematic = false;
+    }
 }
