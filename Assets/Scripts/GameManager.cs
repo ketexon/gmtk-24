537da18c7a0835b6dd7d164faaf95e344d101d9e
hiroglyph22 <hirotrepreneur@gmail.com>
2024-08-19 23:59:17 -0700

    Revert "Added start button that starts point system"
    This reverts commit 09c8e146d61dc55c00fd230662e4d651ebfe4198.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
-    public Button startButton;
     static public bool gameRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        startButton.onClick.AddListener(TaskOnStart);
+        gameRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void TaskOnStart()
-    {
-        gameRunning = true;
     }
 }
